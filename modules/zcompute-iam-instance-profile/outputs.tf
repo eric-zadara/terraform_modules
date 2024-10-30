065d0791d0b9492396fe6b1978ca66a66b8cc5a7
eric-zadara <eric-zadara@users.noreply.github.com>
2024-10-30 14:01:56 -0700

    chore: disable debug output

--- modules/zcompute-iam-instance-profile/outputs.tf
+++ modules/zcompute-iam-instance-profile/outputs.tf
@@ -2,6 +2,6 @@ output "instance_profile_name" {
   description = "In 22.09.x the launch configuration expecting the unique id and not the instance profile name."
   value       = aws_iam_instance_profile.this.unique_id
 }
-output "debug" {
-  value = data.aws_iam_policy_document.policy.json
-}
+#output "debug" {
+#  value = data.aws_iam_policy_document.policy.json
+#}
